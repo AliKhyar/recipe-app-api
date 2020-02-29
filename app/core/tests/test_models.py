6956ae8bd9c7dd9b67e4f052eae105f4a82fb24d
AliKhyar <ali.khyaroxoc@gmail.com>
2020-03-01 00:27:11 +0100

    Setup admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,7 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+        
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None,'test123')
