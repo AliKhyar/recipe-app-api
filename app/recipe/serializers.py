e24ba1fc1959a4e4f61cd9e4c61ad111f87f3f87
AliKhyar <ali.khyaroxoc@gmail.com>
2020-06-29 01:36:41 +0100

    add list tags

---
+++ app/recipe/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializer
+from app.core.models import Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    """serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+        
